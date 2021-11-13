4c39d8250039ee349a17d64c511dbec0f882c14d
hoanghiep3110 <pthh3110@gmail.com>
2021-11-13 19:46:56 +0700

    reup API
    up lại mấy cái api bị lỗi

--- QLNhaTro_API/APIController/PHONGController.cs
+++ QLNhaTro_API/APIController/PHONGController.cs
@@ -1,15 +1,14 @@
 ﻿using QLNhaTro_API.Models;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
 
-namespace QLNhaTro_API.Controllers
+namespace QLNhaTro_API.APIController
 {
     public class PHONGController : ApiController
     {
-        DBQLNhaTro db = new DBQLNhaTro();
+        private DBQLNhaTro db = new DBQLNhaTro();
         // GET: api/PHONG
         public IEnumerable<Phong> Get()
         {
@@ -19,21 +18,40 @@ namespace QLNhaTro_API.Controllers
         // GET: api/PHONG/5
         public Phong Get(int id)
         {
-            return db.Phongs.SingleOrDefault(p=>p.IdPhong == id);
+            return db.Phongs.SingleOrDefault(p => p.IdPhong == id);
         }
 
         // POST: api/PHONG
         public int Post(Phong phong)
         {
-            db.Phongs.Add(phong);
-            db.SaveChanges();
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return 0;
+                }
+                db.Phongs.Add(phong);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
             return 1;
         }
 
         // PUT: api/PHONG/5
         public int Put(int id, Phong newPhong)
         {
+            if (!ModelState.IsValid)
+            {
+                return 0;
+            }
             var phong = db.Phongs.Find(id);
+            if (phong == null)
+            {
+                return -1;
+            }
             phong.TenPhong = newPhong.TenPhong;
             phong.TrangThai = newPhong.TrangThai;
             db.SaveChanges();
---
+++ QLNhaTro_API/APIController/TAIKHOANController.cs
@@ -0,0 +1,75 @@
+﻿using QLNhaTro_API.Helper;
+using QLNhaTro_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace QLNhaTro_API.APIController
+{
+    public class TAIKHOANController : ApiController
+    {
+        DBQLNhaTro db = new DBQLNhaTro();
+        // GET: api/TAIKHOAN
+        public IEnumerable<TaiKhoan> Get()
+        {
+            return db.TaiKhoans.ToList();
+        }
+
+        // GET: api/TAIKHOAN/5
+        public TaiKhoan Get(int id)
+        {
+            return db.TaiKhoans.SingleOrDefault(p => p.IdTaiKhoan == id);
+        }
+
+        // POST: api/TAIKHOAN
+        public int Post(TaiKhoan taiKhoan)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return 0;
+                }
+                db.TaiKhoans.Add(taiKhoan);
+                taiKhoan.Password = MD5Helper.MD5Hash(taiKhoan.Password);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            return 1;
+        }
+
+        // PUT: api/TAIKHOAN/5
+        public int Put(int id, TaiKhoan newTaikhoan)
+        {
+            if (!ModelState.IsValid)
+            {
+                return 0;
+            }
+            var taiKhoan = db.TaiKhoans.Find(id);
+            if(taiKhoan == null)
+            {
+                return -1;
+            }
+            taiKhoan.HoTen = newTaikhoan.HoTen;
+            taiKhoan.Sdt = newTaikhoan.Sdt;
+            taiKhoan.DiaChi = newTaikhoan.DiaChi;
+            taiKhoan.Username = newTaikhoan.Username;
+            taiKhoan.Password = MD5Helper.MD5Hash(newTaikhoan.Password);
+            db.SaveChanges();
+            return 1;
+        }
+
+        // DELETE: api/TAIKHOAN/5
+        public int Delete(int id)
+        {
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(id);
+            db.TaiKhoans.Remove(taiKhoan);
+            db.SaveChanges();
+            return 1;
+        }
+    }
+}
