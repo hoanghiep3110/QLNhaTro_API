026f87c53b45610c8644c6f4e58da7611de702b7
hai2606 <86307572+hai0626@users.noreply.github.com>
2021-11-13 15:43:32 +0700

    upload

--- QLNhaTro_API/App_Start/WebApiConfig.cs
+++ QLNhaTro_API/App_Start/WebApiConfig.cs
@@ -20,6 +20,36 @@ namespace QLNhaTro_API.App_Start
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //config.Routes.MapHttpRoute(
+            //    name: "GetAllRoom",
+            //    routeTemplate: "api/get-room",
+            //    defaults: new { controller = "PHONG", action = "Get" }
+            //);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "GetRoomtDetail",
+            //    routeTemplate: "api/room-detail/{id}",
+            //    defaults: new { controller = "PHONG", action = "Get" }
+            //);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "EditRoom",
+            //    routeTemplate: "api/put/{id}",
+            //    defaults: new { controller = "PHONG", action = "Put" }
+            //);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "AddRoom",
+            //    routeTemplate: "api/post",
+            //    defaults: new { controller = "PHONG", action = "Post" }
+            //);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DeleteRoom",
+            //    routeTemplate: "api/delete/{id}",
+            //    defaults: new { controller = "PHONG", action = "Delete" }
+            //);
         }
     }
 }
